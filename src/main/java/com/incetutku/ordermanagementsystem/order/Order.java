4cb6cf1c29c16eebb30f633ba5288265e21740ad
Tutku Ince <tutku.ince@outlook.com>
2024-11-16 00:11:30 +0300

    Order & OrderRepo

---
+++ src/main/java/com/incetutku/ordermanagementsystem/order/Order.java
@@ -0,0 +1,26 @@
+package com.incetutku.ordermanagementsystem.order;
+
+import com.incetutku.ordermanagementsystem.order.type.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@Table(name = "orders")
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String orderIdentifier;
+    private String customerName;
+    private String customerEmail;
+    private Timestamp orderDate = Timestamp.from(Instant.now());
+    private Status status = Status.OPEN;
+}
---
+++ src/main/java/com/incetutku/ordermanagementsystem/order/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.incetutku.ordermanagementsystem.order;
+
+import org.springframework.data.repository.CrudRepository;
+
+import java.util.Optional;
+
+public interface OrderRepository extends CrudRepository<Order, Long> {
+    Optional<Order> findOrderByOrderIdentifier(String orderIdentifier);
+}
