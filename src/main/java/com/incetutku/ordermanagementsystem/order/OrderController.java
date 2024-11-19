9ad65b915b2cd72bd5b31fa7715293770c4130fb
Tutku Ince <tutku.ince@outlook.com>
2024-11-19 17:33:36 +0300

    Order Controller

---
+++ src/main/java/com/incetutku/ordermanagementsystem/order/OrderController.java
@@ -0,0 +1,25 @@
+package com.incetutku.ordermanagementsystem.order;
+
+import com.incetutku.ordermanagementsystem.order.dto.OrderDto;
+import com.incetutku.ordermanagementsystem.order.dto.OrderResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/orders")
+public class OrderController {
+    private final OrderService orderService;
+
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PostMapping
+    public ResponseEntity<OrderResponseDto> createOrder(@RequestBody OrderDto orderDto) {
+        return new ResponseEntity<>(orderService.createOrder(orderDto), HttpStatus.CREATED);
+    }
+}
