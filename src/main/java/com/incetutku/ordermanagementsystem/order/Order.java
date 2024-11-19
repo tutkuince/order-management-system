31bae763e54e909083af396a9e3974f3455b91fd
Tutku Ince <tutku.ince@outlook.com>
2024-11-19 17:23:28 +0300

    Added Builder annotation

--- src/main/java/com/incetutku/ordermanagementsystem/order/Order.java
+++ src/main/java/com/incetutku/ordermanagementsystem/order/Order.java
@@ -2,6 +2,7 @@ package com.incetutku.ordermanagementsystem.order;
 
 import com.incetutku.ordermanagementsystem.order.type.Status;
 import jakarta.persistence.*;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -12,6 +13,7 @@ import java.time.Instant;
 @Getter
 @Setter
 @ToString
+@Builder
 @Entity
 @Table(name = "orders")
 public class Order {
