3dc313d03ab82c8c8331b4548e8914b5d30b9af8
Tutku Ince <tutku.ince@outlook.com>
2024-11-19 19:35:47 +0300

    create order integration test

--- src/test/java/com/incetutku/ordermanagementsystem/order/OrderIntegrationTest.java
+++ src/test/java/com/incetutku/ordermanagementsystem/order/OrderIntegrationTest.java
@@ -1,16 +1,40 @@
 package com.incetutku.ordermanagementsystem.order;
 
+import com.incetutku.ordermanagementsystem.order.dto.InventoryRequestDto;
+import com.incetutku.ordermanagementsystem.order.dto.OrderDto;
+import com.incetutku.ordermanagementsystem.order.dto.OrderResponseDto;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.modulith.test.ApplicationModuleTest;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.List;
 
-@ApplicationModuleTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ApplicationModuleTest(mode = ApplicationModuleTest.BootstrapMode.DIRECT_DEPENDENCIES)
 class OrderIntegrationTest {
+
+    @Autowired
+    OrderService orderService;
 
     @Test
     void verifyModule() {
 
     }
 
+    @Test
+    void createOrder() {
+        List<InventoryRequestDto> requestDtoList = List.of(
+                new InventoryRequestDto("ruller", 1),
+                new InventoryRequestDto("pencil", 3)
+        );
+
+        OrderDto orderDto = new OrderDto("Test Man", "test@test.com", requestDtoList);
+
+        OrderResponseDto order = orderService.createOrder(orderDto);
+
+        assertThat(order.message()).isEqualTo("Order Currently Processed");
+        assertThat(order.statusCode()).isEqualTo(102);
+
+    }
 }
