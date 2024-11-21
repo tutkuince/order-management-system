5a03c323401b9cc27a02626604b289d1690e7940
Tutku Ince <tutku.ince@outlook.com>
2024-11-21 16:28:58 +0300

    Enum

---
+++ src/main/java/com/incetutku/ordermanagementsystem/event/action/Action.java
@@ -0,0 +1,23 @@
+package com.incetutku.ordermanagementsystem.event.action;
+
+import java.util.Arrays;
+
+public enum Action {
+    PAYMENT("P"), EMAIL("E");
+
+    private String code;
+
+    Action(String code) {
+        this.code = code;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public static Action getActionByName(final String name) {
+        return Arrays.stream(Action.values())
+                .filter(action -> action.name().equalsIgnoreCase(name))
+                .findFirst().orElse(null);
+    }
+}
